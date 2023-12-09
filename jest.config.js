8e79bc1ed3afbb3ee78f2e7d216d1f08f9a64f63
anthonycole <anthony.cole@aesop.com>
2023-12-10 01:55:50 +1100

    feat(Robot): Surpress console log messages

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   roots: ['<rootDir>/src'],
+  setupFiles: ['<rootDir>/jestSetup.ts'],
   testMatch: ['**/__tests__/**/*.+(ts|tsx|js)', '**/?(*.)+(spec|test).+(ts|tsx|js)'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
